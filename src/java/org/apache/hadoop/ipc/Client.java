41726c8f2cd4967690b840e110c2c5d67f98e6d6
Thomas White <tomwhite@apache.org>
2010-04-28 17:34:55 +0000

    HADOOP-6724. IPC doesn't properly handle IOEs thrown by socket factory. Contributed by Todd Lipcon.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@939018 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: cd85fc46dc4d027a2102e874051d943fab0512ed

--- src/java/org/apache/hadoop/ipc/Client.java
+++ src/java/org/apache/hadoop/ipc/Client.java
@@ -2199,6 +2199,13 @@ throws|throws
 name|IOException
 block|{
 comment|// close the current connection
+if|if
+condition|(
+name|socket
+operator|!=
+literal|null
+condition|)
+block|{
 try|try
 block|{
 name|socket
@@ -2222,6 +2229,7 @@ argument_list|,
 name|e
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 comment|// set socket to null so that the next call to setupIOstreams
 comment|// can start the process of connect all over again.
