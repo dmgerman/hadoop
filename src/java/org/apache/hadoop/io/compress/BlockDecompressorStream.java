15e9d0e2601c9413a268539d6ee14010a7d34089
Thomas White <tomwhite@apache.org>
2010-10-25 23:50:05 +0000

    Reverting HADOOP-6663.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1027316 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3d27eaad25a999c403216107a95de9dd06143361

--- src/java/org/apache/hadoop/io/compress/BlockDecompressorStream.java
+++ src/java/org/apache/hadoop/io/compress/BlockDecompressorStream.java
@@ -225,24 +225,6 @@ name|noUncompressedBytes
 operator|=
 literal|0
 expr_stmt|;
-comment|// EOF if originalBlockSize is 0
-comment|// This will occur only when decompressing previous compressed empty file
-if|if
-condition|(
-name|originalBlockSize
-operator|==
-literal|0
-condition|)
-block|{
-name|eof
-operator|=
-literal|true
-expr_stmt|;
-return|return
-operator|-
-literal|1
-return|;
-block|}
 block|}
 name|int
 name|n
