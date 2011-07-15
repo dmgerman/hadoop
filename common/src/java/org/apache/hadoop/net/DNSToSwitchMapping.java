601959cbe9a20bc100b44f7efce837efa5751940
Jitendra Nath Pandey <jitendra@apache.org>
2011-07-15 20:58:00 +0000

    HADOOP-7314. Add support for throwing UnknownHostException when a host doesn't resolve. Contributed by Jeffrey Naisbitt.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1147317 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 31f1a674e59207fe11065e8026fa310bdcc9c924

--- common/src/java/org/apache/hadoop/net/DNSToSwitchMapping.java
+++ common/src/java/org/apache/hadoop/net/DNSToSwitchMapping.java
@@ -28,6 +28,16 @@ end_import
 
 begin_import
 import|import
+name|java
+operator|.
+name|net
+operator|.
+name|UnknownHostException
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -87,6 +97,24 @@ name|String
 argument_list|>
 name|names
 parameter_list|)
+function_decl|;
+comment|/**    * Resolves a list of DNS-names/IP-addresses and returns back a list of    * switch information (network paths). One-to-one correspondence must be     * maintained between the elements in the lists.     * Consider an element in the argument list - x.y.com. The switch information    * that is returned must be a network path of the form /foo/rack,     * where / is the root, and 'foo' is the switch where 'rack' is connected.    * Note the hostname/ip-address is not part of the returned path.    * The network topology of the cluster would determine the number of    * components in the network path.  Unlike 'resolve', names must be     * resolvable    * @param names    * @return list of resolved network paths    * @throws UnknownHostException if any hosts are not resolvable    */
+DECL|method|resolveValidHosts (List<String> names)
+specifier|public
+name|List
+argument_list|<
+name|String
+argument_list|>
+name|resolveValidHosts
+parameter_list|(
+name|List
+argument_list|<
+name|String
+argument_list|>
+name|names
+parameter_list|)
+throws|throws
+name|UnknownHostException
 function_decl|;
 block|}
 end_interface
