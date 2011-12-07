4ce3f3f8d913d02f0964a020ea13dbfd08e24e1b
Mahadev Konar <mahadev@apache.org>
2011-12-07 00:29:45 +0000

    MAPREDUCE-3147. Handle leaf queues with the same name properly. (Ravi Prakash via mahadev)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1211243 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ec87e163d418b3fc3d9456e844143d6c61c80a48

--- hadoop-mapreduce-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/TestApplicationLimits.java
+++ hadoop-mapreduce-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/TestApplicationLimits.java
@@ -52,6 +52,16 @@ begin_import
 import|import
 name|java
 operator|.
+name|io
+operator|.
+name|IOException
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
 name|util
 operator|.
 name|HashMap
@@ -295,6 +305,8 @@ specifier|public
 name|void
 name|setUp
 parameter_list|()
+throws|throws
+name|IOException
 block|{
 name|CapacitySchedulerConfiguration
 name|csConf
