6ce7d015b233193d4ba293e0dea1f0925cb03b17
Suresh Srinivas <suresh@apache.org>
2012-02-15 22:00:08 +0000

    HDFS-2938. Recursive delete of a large directory make namenode unresponsive. Contributed by Hari Mankude.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1244752 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ff91453227488902d8c730d69c90fb1a97b35d75

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
@@ -10452,24 +10452,12 @@ operator|.
 name|logSync
 argument_list|()
 expr_stmt|;
-name|writeLock
-argument_list|()
-expr_stmt|;
-try|try
-block|{
 name|removeBlocks
 argument_list|(
 name|collectedBlocks
 argument_list|)
 expr_stmt|;
 comment|// Incremental deletion of blocks
-block|}
-finally|finally
-block|{
-name|writeUnlock
-argument_list|()
-expr_stmt|;
-block|}
 name|collectedBlocks
 operator|.
 name|clear
@@ -10503,7 +10491,7 @@ return|return
 literal|true
 return|;
 block|}
-comment|/** From the given list, incrementally remove the blocks from blockManager */
+comment|/**     * From the given list, incrementally remove the blocks from blockManager    * Writelock is dropped and reacquired every BLOCK_DELETION_INCREMENT to    * ensure that other waiters on the lock can get in. See HDFS-2938    */
 DECL|method|removeBlocks (List<Block> blocks)
 specifier|private
 name|void
@@ -10516,10 +10504,6 @@ argument_list|>
 name|blocks
 parameter_list|)
 block|{
-assert|assert
-name|hasWriteLock
-argument_list|()
-assert|;
 name|int
 name|start
 init|=
@@ -10562,6 +10546,11 @@ argument_list|()
 else|:
 name|end
 expr_stmt|;
+name|writeLock
+argument_list|()
+expr_stmt|;
+try|try
+block|{
 for|for
 control|(
 name|int
@@ -10588,6 +10577,13 @@ argument_list|(
 name|i
 argument_list|)
 argument_list|)
+expr_stmt|;
+block|}
+block|}
+finally|finally
+block|{
+name|writeUnlock
+argument_list|()
 expr_stmt|;
 block|}
 name|start
