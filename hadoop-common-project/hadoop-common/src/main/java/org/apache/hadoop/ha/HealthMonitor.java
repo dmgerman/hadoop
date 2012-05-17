0e3b3c786abf1d2c4b125ccefbb468baa3c9e583
Todd Lipcon <todd@apache.org>
2012-05-17 00:36:13 +0000

    HDFS-3432. TestDFSZKFailoverController tries to fail over too early. Contributed by Todd Lipcon.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/HDFS-3042@1339434 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0600e834be8a148c8727ce4520e48b7f3235a738

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ha/HealthMonitor.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ha/HealthMonitor.java
@@ -62,6 +62,20 @@ name|org
 operator|.
 name|apache
 operator|.
+name|hadoop
+operator|.
+name|classification
+operator|.
+name|InterfaceAudience
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
 name|commons
 operator|.
 name|logging
@@ -205,7 +219,12 @@ comment|/**  * This class is a daemon which runs in a loop, periodically heartbe
 end_comment
 
 begin_class
+annotation|@
+name|InterfaceAudience
+operator|.
+name|Private
 DECL|class|HealthMonitor
+specifier|public
 class|class
 name|HealthMonitor
 block|{
@@ -320,7 +339,12 @@ operator|.
 name|INITIALIZING
 argument_list|)
 decl_stmt|;
+annotation|@
+name|InterfaceAudience
+operator|.
+name|Private
 DECL|enum|State
+specifier|public
 enum|enum
 name|State
 block|{
