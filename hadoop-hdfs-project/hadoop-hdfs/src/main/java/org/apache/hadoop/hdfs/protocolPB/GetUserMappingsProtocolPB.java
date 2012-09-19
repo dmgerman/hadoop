79e7bd45495e0496b47db8c8035876ae3645cd66
Alejandro Abdelnur <tucu@apache.org>
2012-09-19 18:06:42 +0000

    Reverting HADOOP-8805
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1387697 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8bf6b80f2bd526157643bee0ad62894435ff6983

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/protocolPB/GetUserMappingsProtocolPB.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/protocolPB/GetUserMappingsProtocolPB.java
@@ -1,10 +1,10 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
-DECL|package|org.apache.hadoop.tools
+DECL|package|org.apache.hadoop.hdfs.protocolPB
 package|package
 name|org
 operator|.
@@ -12,7 +12,9 @@ name|apache
 operator|.
 name|hadoop
 operator|.
-name|tools
+name|hdfs
+operator|.
+name|protocolPB
 package|;
 end_package
 
@@ -66,6 +68,26 @@ name|apache
 operator|.
 name|hadoop
 operator|.
+name|hdfs
+operator|.
+name|protocol
+operator|.
+name|proto
+operator|.
+name|GetUserMappingsProtocolProtos
+operator|.
+name|GetUserMappingsProtocolService
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|hadoop
+operator|.
 name|ipc
 operator|.
 name|ProtocolInfo
@@ -83,24 +105,6 @@ operator|.
 name|security
 operator|.
 name|KerberosInfo
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|hadoop
-operator|.
-name|tools
-operator|.
-name|proto
-operator|.
-name|GetUserMappingsProtocol
-operator|.
-name|GetUserMappingsProtocolService
 import|;
 end_import
 
@@ -134,8 +138,6 @@ block|{
 literal|"HDFS"
 block|,
 literal|"MapReduce"
-block|,
-literal|"YARN"
 block|}
 argument_list|)
 annotation|@
