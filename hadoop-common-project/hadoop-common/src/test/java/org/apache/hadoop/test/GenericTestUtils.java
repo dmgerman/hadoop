31869c4da7e98f6c20ded3159eec7d529a0df577
Todd Lipcon <todd@apache.org>
2012-10-04 01:40:12 +0000

    HDFS-4004. TestJournalNode#testJournal fails because of test case execution order. Contributed by Chao Shi.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/HDFS-3077@1393870 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 10a0eaaf0a5ded24f95d93a1f609320db322494a

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/test/GenericTestUtils.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/test/GenericTestUtils.java
@@ -106,6 +106,20 @@ name|java
 operator|.
 name|util
 operator|.
+name|concurrent
+operator|.
+name|atomic
+operator|.
+name|AtomicInteger
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
 name|regex
 operator|.
 name|Pattern
@@ -307,6 +321,17 @@ specifier|abstract
 class|class
 name|GenericTestUtils
 block|{
+DECL|field|sequence
+specifier|private
+specifier|static
+specifier|final
+name|AtomicInteger
+name|sequence
+init|=
+operator|new
+name|AtomicInteger
+argument_list|()
+decl_stmt|;
 comment|/**    * Extracts the name of the method where the invocation has happened    * @return String name of the invoking method    */
 DECL|method|getMethodName ()
 specifier|public
@@ -328,6 +353,21 @@ literal|2
 index|]
 operator|.
 name|getMethodName
+argument_list|()
+return|;
+block|}
+comment|/**    * Generates a process-wide unique sequence number.    * @return an unique sequence number    */
+DECL|method|uniqueSequenceId ()
+specifier|public
+specifier|static
+name|int
+name|uniqueSequenceId
+parameter_list|()
+block|{
+return|return
+name|sequence
+operator|.
+name|incrementAndGet
 argument_list|()
 return|;
 block|}
