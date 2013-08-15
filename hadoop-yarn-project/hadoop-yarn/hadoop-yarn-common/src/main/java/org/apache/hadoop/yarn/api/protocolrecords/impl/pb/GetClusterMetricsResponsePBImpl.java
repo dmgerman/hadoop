a74cc3b5ab94bb9fd98ba5beda06fbf62c057aa3
Siddharth Seth <sseth@apache.org>
2013-08-15 07:20:14 +0000

    YARN-1045. Improve toString implementation for PBImpls. Contributed by Jian He.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1514185 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b776bd46aed2f5b3aa226af36c0081a7d1f69eda

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/api/protocolrecords/impl/pb/GetClusterMetricsResponsePBImpl.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/api/protocolrecords/impl/pb/GetClusterMetricsResponsePBImpl.java
@@ -168,6 +168,18 @@ name|GetClusterMetricsResponseProtoOrBuilder
 import|;
 end_import
 
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|protobuf
+operator|.
+name|TextFormat
+import|;
+end_import
+
 begin_class
 annotation|@
 name|Private
@@ -358,24 +370,12 @@ name|toString
 parameter_list|()
 block|{
 return|return
+name|TextFormat
+operator|.
+name|shortDebugString
+argument_list|(
 name|getProto
 argument_list|()
-operator|.
-name|toString
-argument_list|()
-operator|.
-name|replaceAll
-argument_list|(
-literal|"\\n"
-argument_list|,
-literal|", "
-argument_list|)
-operator|.
-name|replaceAll
-argument_list|(
-literal|"\\s+"
-argument_list|,
-literal|" "
 argument_list|)
 return|;
 block|}
--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/api/records/impl/pb/ApplicationReportPBImpl.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/api/records/impl/pb/ApplicationReportPBImpl.java
@@ -326,6 +326,18 @@ name|YarnApplicationStateProto
 import|;
 end_import
 
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|protobuf
+operator|.
+name|TextFormat
+import|;
+end_import
+
 begin_class
 annotation|@
 name|Private
@@ -1922,24 +1934,12 @@ name|toString
 parameter_list|()
 block|{
 return|return
+name|TextFormat
+operator|.
+name|shortDebugString
+argument_list|(
 name|getProto
 argument_list|()
-operator|.
-name|toString
-argument_list|()
-operator|.
-name|replaceAll
-argument_list|(
-literal|"\\n"
-argument_list|,
-literal|", "
-argument_list|)
-operator|.
-name|replaceAll
-argument_list|(
-literal|"\\s+"
-argument_list|,
-literal|" "
 argument_list|)
 return|;
 block|}
