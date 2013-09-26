7d8f47875a7722c4c1f65191cc5159b9aa004502
Chris Nauroth <cnauroth@apache.org>
2013-09-26 20:30:14 +0000

    HDFS-5266. ElasticByteBufferPool#Key does not implement equals. Contributed by Chris Nauroth.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/HDFS-4949@1526671 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8f17d645273a30f829bf16f22d8a7c6a9710d627

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/ByteBufferPool.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/ByteBufferPool.java
@@ -68,7 +68,7 @@ specifier|public
 interface|interface
 name|ByteBufferPool
 block|{
-comment|/**    * Get a new direct ByteBuffer.  The pool can provide this from    * removing a buffer from its internal cache, or by allocating a     * new buffer.    *    * @param direct     Whether the buffer should be direct.    * @param minLength  The minimum length the buffer will have.    * @return           A new ByteBuffer.  This ByteBuffer must be direct.    *                   Its capacity can be less than what was requested, but    *                   must be at least 1 byte.    */
+comment|/**    * Get a new direct ByteBuffer.  The pool can provide this from    * removing a buffer from its internal cache, or by allocating a     * new buffer.    *    * @param direct     Whether the buffer should be direct.    * @param length     The minimum length the buffer will have.    * @return           A new ByteBuffer.  This ByteBuffer must be direct.    *                   Its capacity can be less than what was requested, but    *                   must be at least 1 byte.    */
 DECL|method|getBuffer (boolean direct, int length)
 name|ByteBuffer
 name|getBuffer
