e27598d62aac78e8604f1aeadf73e28a878290c5
Haohui Mai <wheat9@apache.org>
2014-04-09 04:02:46 +0000

    HADOOP-10474. Move o.a.h.record to hadoop-streaming. Contributed by Haohui Mai.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1585886 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8ca32df08e5021d144ebfa8b85da7879143c01ae

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/security/SecureShuffleUtils.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/security/SecureShuffleUtils.java
@@ -186,20 +186,6 @@ end_import
 
 begin_import
 import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|hadoop
-operator|.
-name|record
-operator|.
-name|Utils
-import|;
-end_import
-
-begin_import
-import|import
 name|com
 operator|.
 name|google
