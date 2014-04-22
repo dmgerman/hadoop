b06600986f321b2143040d952d6c1c89277577b6
Chris Nauroth <cnauroth@apache.org>
2014-04-22 19:47:53 +0000

    HADOOP-10503. Move junit up to v 4.11. Contributed by Chris Nauroth.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1589273 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 7bd571859404bf71edff61df91dfeac8e6fb3d2f

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/TestFileSystemCaching.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/TestFileSystemCaching.java
@@ -18,9 +18,9 @@ end_package
 
 begin_import
 import|import static
-name|junit
+name|org
 operator|.
-name|framework
+name|junit
 operator|.
 name|Assert
 operator|.
@@ -30,9 +30,9 @@ end_import
 
 begin_import
 import|import static
-name|junit
+name|org
 operator|.
-name|framework
+name|junit
 operator|.
 name|Assert
 operator|.
