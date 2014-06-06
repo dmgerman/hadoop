5cfa4f92096bfd76e5f4b49a326ae771542a137a
Uma Maheswara Rao G <umamahesh@apache.org>
2014-06-06 03:48:43 +0000

    HDFS-6464. Support multiple xattr.name parameters for WebHDFS getXAttrs. Contributed by Yi Liu.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1600810 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6a4f6d6b3e41e660c214b77c9ed43d4c65200b6a

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/web/resources/GetOpParam.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/web/resources/GetOpParam.java
@@ -151,16 +151,6 @@ operator|.
 name|HTTP_OK
 argument_list|)
 block|,
-DECL|enumConstant|GETXATTR
-name|GETXATTR
-argument_list|(
-literal|false
-argument_list|,
-name|HttpURLConnection
-operator|.
-name|HTTP_OK
-argument_list|)
-block|,
 DECL|enumConstant|GETXATTRS
 name|GETXATTRS
 argument_list|(
