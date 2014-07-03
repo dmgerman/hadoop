84be44acc1a32e406d51a9348c34854f4002b74f
Steve Loughran <stevel@apache.org>
2014-07-03 12:09:45 +0000

    HADOOP-9361: Strictly define FileSystem APIs - HDFS portion
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1607597 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b6fbaba8e98d2da913cbe5474cab479cd4efd191

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSInputStream.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSInputStream.java
@@ -22,6 +22,16 @@ name|java
 operator|.
 name|io
 operator|.
+name|EOFException
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|io
+operator|.
 name|IOException
 import|;
 end_import
@@ -6718,7 +6728,7 @@ condition|)
 block|{
 throw|throw
 operator|new
-name|IOException
+name|EOFException
 argument_list|(
 literal|"Cannot seek after EOF"
 argument_list|)
@@ -6733,7 +6743,7 @@ condition|)
 block|{
 throw|throw
 operator|new
-name|IOException
+name|EOFException
 argument_list|(
 literal|"Cannot seek to negative offset"
 argument_list|)
