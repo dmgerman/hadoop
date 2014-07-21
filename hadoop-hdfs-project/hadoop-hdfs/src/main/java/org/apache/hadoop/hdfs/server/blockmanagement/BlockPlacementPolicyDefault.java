3ff2697ba02659167700d73e2235f45503cca679
Tsz-wo Sze <szetszwo@apache.org>
2014-07-21 23:21:12 +0000

    HDFS-6680. BlockPlacementPolicyDefault does not choose favored nodes correctly.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1612427 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 44d9bb26d640ca5c1de651563c7993b4ecd6b653

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/BlockPlacementPolicyDefault.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/BlockPlacementPolicyDefault.java
@@ -773,17 +773,17 @@ literal|0
 init|;
 name|i
 operator|<
-name|Math
-operator|.
-name|min
-argument_list|(
 name|favoredNodes
 operator|.
 name|size
 argument_list|()
-argument_list|,
+operator|&&
+name|results
+operator|.
+name|size
+argument_list|()
+operator|<
 name|numOfReplicas
-argument_list|)
 condition|;
 name|i
 operator|++
@@ -831,6 +831,8 @@ argument_list|,
 name|avoidStaleNodes
 argument_list|,
 name|storageType
+argument_list|,
+literal|false
 argument_list|)
 decl_stmt|;
 if|if
@@ -1463,6 +1465,8 @@ argument_list|,
 name|avoidStaleNodes
 argument_list|,
 name|storageType
+argument_list|,
+literal|true
 argument_list|)
 operator|.
 name|getDatanodeDescriptor
@@ -1795,7 +1799,7 @@ name|writer
 return|;
 block|}
 comment|/**    * Choose<i>localMachine</i> as the target.    * if<i>localMachine</i> is not available,     * choose a node on the same rack    * @return the chosen storage    */
-DECL|method|chooseLocalStorage (Node localMachine, Set<Node> excludedNodes, long blocksize, int maxNodesPerRack, List<DatanodeStorageInfo> results, boolean avoidStaleNodes, StorageType storageType)
+DECL|method|chooseLocalStorage (Node localMachine, Set<Node> excludedNodes, long blocksize, int maxNodesPerRack, List<DatanodeStorageInfo> results, boolean avoidStaleNodes, StorageType storageType, boolean fallbackToLocalRack)
 specifier|protected
 name|DatanodeStorageInfo
 name|chooseLocalStorage
@@ -1826,6 +1830,9 @@ name|avoidStaleNodes
 parameter_list|,
 name|StorageType
 name|storageType
+parameter_list|,
+name|boolean
+name|fallbackToLocalRack
 parameter_list|)
 throws|throws
 name|NotEnoughReplicasException
@@ -1837,6 +1844,7 @@ name|localMachine
 operator|==
 literal|null
 condition|)
+block|{
 return|return
 name|chooseRandom
 argument_list|(
@@ -1857,6 +1865,7 @@ argument_list|,
 name|storageType
 argument_list|)
 return|;
+block|}
 if|if
 condition|(
 name|preferLocalNode
@@ -1932,6 +1941,16 @@ return|;
 block|}
 block|}
 block|}
+block|}
+if|if
+condition|(
+operator|!
+name|fallbackToLocalRack
+condition|)
+block|{
+return|return
+literal|null
+return|;
 block|}
 comment|// try a node on local rack
 return|return
