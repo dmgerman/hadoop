d3235483019396ddc82eaac2796f63ce178700b9
Arpit Agarwal <arp@apache.org>
2014-08-08 21:20:19 +0000

    HDFS-6830. BlockInfo.addStorage fails when DN changes the storage for a block replica. (Arpit Agarwal)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1616883 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a70c9de3f1fcb5930a5eab5a92085055d79edbf5

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/BlockManager.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/BlockManager.java
@@ -9703,14 +9703,13 @@ operator|.
 name|FINALIZED
 operator|&&
 operator|(
+operator|!
 name|storedBlock
 operator|.
 name|findDatanode
 argument_list|(
 name|dn
 argument_list|)
-operator|<
-literal|0
 operator|||
 name|corruptReplicas
 operator|.
@@ -10479,6 +10478,7 @@ name|ReplicaState
 operator|.
 name|FINALIZED
 operator|&&
+operator|!
 name|block
 operator|.
 name|findDatanode
@@ -10488,8 +10488,6 @@ operator|.
 name|getDatanodeDescriptor
 argument_list|()
 argument_list|)
-operator|<
-literal|0
 condition|)
 block|{
 name|addStoredBlock
