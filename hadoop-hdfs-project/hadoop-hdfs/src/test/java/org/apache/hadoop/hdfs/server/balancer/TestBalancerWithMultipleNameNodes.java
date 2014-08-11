9e4f543d3f515062d993314d78135d1a2814c383
Jing Zhao <jing9@apache.org>
2014-08-11 18:01:14 +0000

    HDFS-6837. Code cleanup for Balancer and Dispatcher. Contributed by Tsz Wo Nicholas Sze.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1617337 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e60673697d5046c29c52bbabdfe80506f99773e4

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/balancer/TestBalancerWithMultipleNameNodes.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/balancer/TestBalancerWithMultipleNameNodes.java
@@ -1247,13 +1247,12 @@ name|Assert
 operator|.
 name|assertEquals
 argument_list|(
-name|Balancer
-operator|.
-name|ReturnStatus
+name|ExitStatus
 operator|.
 name|SUCCESS
 operator|.
-name|code
+name|getExitCode
+argument_list|()
 argument_list|,
 name|r
 argument_list|)
