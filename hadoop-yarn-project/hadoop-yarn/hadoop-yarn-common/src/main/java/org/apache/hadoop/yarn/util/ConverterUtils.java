a5668d8a5f24ef564d46145c4ece7de155b3be35
Tsuyoshi Ozawa <ozawa@apache.org>
2015-02-12 18:39:29 +0900

    YARN-3157. Refactor the exception handling in ConverterUtils#to*Id. Contributed by Bibin A Chundatt.
    Former-commit-id: 95a41bf35d8ba0a1ec087f456914231103d98fb9

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/util/ConverterUtils.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/util/ConverterUtils.java
@@ -110,6 +110,16 @@ end_import
 
 begin_import
 import|import
+name|java
+operator|.
+name|util
+operator|.
+name|NoSuchElementException
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -1075,6 +1085,8 @@ name|String
 name|containerIdStr
 parameter_list|)
 block|{
+try|try
+block|{
 return|return
 name|ContainerId
 operator|.
@@ -1083,6 +1095,25 @@ argument_list|(
 name|containerIdStr
 argument_list|)
 return|;
+block|}
+catch|catch
+parameter_list|(
+name|NoSuchElementException
+name|e
+parameter_list|)
+block|{
+throw|throw
+operator|new
+name|IllegalArgumentException
+argument_list|(
+literal|"Invalid ContainerId: "
+operator|+
+name|containerIdStr
+argument_list|,
+name|e
+argument_list|)
+throw|;
+block|}
 block|}
 DECL|method|toApplicationAttemptId ( String applicationAttmeptIdStr)
 specifier|public
@@ -1156,6 +1187,24 @@ operator|+
 name|applicationAttmeptIdStr
 argument_list|,
 name|n
+argument_list|)
+throw|;
+block|}
+catch|catch
+parameter_list|(
+name|NoSuchElementException
+name|e
+parameter_list|)
+block|{
+throw|throw
+operator|new
+name|IllegalArgumentException
+argument_list|(
+literal|"Invalid AppAttemptId: "
+operator|+
+name|applicationAttmeptIdStr
+argument_list|,
+name|e
 argument_list|)
 throw|;
 block|}
@@ -1236,6 +1285,24 @@ operator|+
 name|appIdStr
 argument_list|,
 name|n
+argument_list|)
+throw|;
+block|}
+catch|catch
+parameter_list|(
+name|NoSuchElementException
+name|e
+parameter_list|)
+block|{
+throw|throw
+operator|new
+name|IllegalArgumentException
+argument_list|(
+literal|"Invalid ApplicationId: "
+operator|+
+name|appIdStr
+argument_list|,
+name|e
 argument_list|)
 throw|;
 block|}
