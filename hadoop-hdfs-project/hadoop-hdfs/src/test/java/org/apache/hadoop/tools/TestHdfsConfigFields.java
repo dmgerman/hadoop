ae500d7f7708da072bf17b3dc27046cd02f9bb7d
Vinayakumar B <vinayakumarb@apache.org>
2015-05-12 12:09:13 +0530

    HDFS-8362. Java Compilation Error in TestHdfsConfigFields.java (Contributed by Arshad Mohammad)
    Former-commit-id: 987abc99b0309a07f0a342746b2a5048d5c36ce0

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/tools/TestHdfsConfigFields.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/tools/TestHdfsConfigFields.java
@@ -4,15 +4,13 @@ comment|/**  * Licensed to the Apache Software Foundation (ASF) under one  * or 
 end_comment
 
 begin_package
-DECL|package|org.apache.hadoop.hdfs.tools
+DECL|package|org.apache.hadoop.tools
 package|package
 name|org
 operator|.
 name|apache
 operator|.
 name|hadoop
-operator|.
-name|hdfs
 operator|.
 name|tools
 package|;
