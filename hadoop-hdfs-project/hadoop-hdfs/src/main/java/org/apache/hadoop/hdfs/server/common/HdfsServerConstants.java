94444cc5a7a399f9c2b0b3662de555203a98b0ef
Akira Ajisaka <aajisaka@apache.org>
2015-05-21 17:30:43 +0900

    HDFS-4383. Document the lease limits. Contributed by Arshad Mohammad.
    Former-commit-id: dc8434ab2b177ca9673bd8eecf7b185d4c4ffb31

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/common/HdfsServerConstants.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/common/HdfsServerConstants.java
@@ -238,9 +238,7 @@ name|MIN_BLOCKS_FOR_WRITE
 init|=
 literal|1
 decl_stmt|;
-comment|//
-comment|// Timeouts, constants
-comment|//
+comment|/**    * For a HDFS client to write to a file, a lease is granted; During the lease    * period, no other client can write to the file. The writing client can    * periodically renew the lease. When the file is closed, the lease is    * revoked. The lease duration is bound by this soft limit and a    * {@link HdfsServerConstants#LEASE_HARDLIMIT_PERIOD hard limit}. Until the    * soft limit expires, the writer has sole write access to the file. If the    * soft limit expires and the client fails to close the file or renew the    * lease, another client can preempt the lease.    */
 DECL|field|LEASE_SOFTLIMIT_PERIOD
 name|long
 name|LEASE_SOFTLIMIT_PERIOD
@@ -249,6 +247,7 @@ literal|60
 operator|*
 literal|1000
 decl_stmt|;
+comment|/**    * For a HDFS client to write to a file, a lease is granted; During the lease    * period, no other client can write to the file. The writing client can    * periodically renew the lease. When the file is closed, the lease is    * revoked. The lease duration is bound by a    * {@link HdfsServerConstants#LEASE_SOFTLIMIT_PERIOD soft limit} and this hard    * limit. If after the hard limit expires and the client has failed to renew    * the lease, HDFS assumes that the client has quit and will automatically    * close the file on behalf of the writer, and recover the lease.    */
 DECL|field|LEASE_HARDLIMIT_PERIOD
 name|long
 name|LEASE_HARDLIMIT_PERIOD
