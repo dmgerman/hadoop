ab4c904a91eb90319a8102ecfd12f093987775ba
Rohith Sharma K S <rohithsharmaks@apache.org>
2016-01-29 11:51:47 +0530

    YARN-4219. addendum patch to fix javadoc errors
    Former-commit-id: 09d831c95ba18e2892cddd749f6e06f112dda7f5

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-timeline-pluginstorage/src/main/java/org/apache/hadoop/yarn/server/timeline/LevelDBCacheTimelineStore.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-timeline-pluginstorage/src/main/java/org/apache/hadoop/yarn/server/timeline/LevelDBCacheTimelineStore.java
@@ -295,7 +295,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * LevelDB implementation of {@link KeyValueBasedTimelineStore}. This  * implementation stores the entity hash map into a LevelDB instance.  * There are two partitions of the key space. One partition is to store a  * entity id to start time mapping:  *  * i!ENTITY_ID!ENTITY_TYPE -> ENTITY_START_TIME  *  * The other partition is to store the actual data:  *  * e!START_TIME!ENTITY_ID!ENTITY_TYPE -> ENTITY_BYTES  *  * This storage does not have any garbage collection mechanism, and is designed  * mainly for caching usages.  */
+comment|/**  * LevelDB implementation of {@link KeyValueBasedTimelineStore}. This  * implementation stores the entity hash map into a LevelDB instance.  * There are two partitions of the key space. One partition is to store a  * entity id to start time mapping:  *  * i!ENTITY_ID!ENTITY_TYPE to ENTITY_START_TIME  *  * The other partition is to store the actual data:  *  * e!START_TIME!ENTITY_ID!ENTITY_TYPE to ENTITY_BYTES  *  * This storage does not have any garbage collection mechanism, and is designed  * mainly for caching usages.  */
 end_comment
 
 begin_class
