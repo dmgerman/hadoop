f6301fb955cdf2331a51aab3c237c6cee262e1b3
Anu Engineer <aengineer@apache.org>
2017-06-09 10:44:07 -0700

    HDFS-11845. Ozone: Output error when DN handshakes with SCM. Contributed by Weiwei Yang
    Former-commit-id: 938728744c7e1af7ccf78dfc2d46a1d302cfacb5

--- hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/scm/ScmConfigKeys.java
+++ hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/scm/ScmConfigKeys.java
@@ -460,7 +460,7 @@ specifier|final
 name|long
 name|OZONE_SCM_HEARTBEAT_RPC_TIMEOUT_DEFAULT
 init|=
-literal|100
+literal|1000
 decl_stmt|;
 comment|/**    * Defines how frequently we will log the missing of heartbeat to a specific    * SCM. In the default case we will write a warning message for each 10    * sequential heart beats that we miss to a specific SCM. This is to avoid    * overrunning the log with lots of HB missed Log statements.    */
 DECL|field|OZONE_SCM_HEARTBEAT_LOG_WARN_INTERVAL_COUNT
