bf5443cf159ec5427eefade29d56fd9375244009
Anu Engineer <aengineer@apache.org>
2017-07-14 15:28:50 -0700

    HDFS-12123. Ozone: OzoneClient: Abstraction of OzoneClient and default implementation. Contributed by Nandakumar.
    Former-commit-id: ab3d510c1def1f7bb4ecfab2dee8e189ce3f4f33

---
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/ozone/OzoneKey.java
@@ -0,0 +1,221 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  *  with the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  *  Unless required by applicable law or agreed to in writing, software  *  distributed under the License is distributed on an "AS IS" BASIS,  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *  See the License for the specific language governing permissions and  *  limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|org.apache.hadoop.ozone
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|hadoop
+operator|.
+name|ozone
+package|;
+end_package
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|hadoop
+operator|.
+name|ksm
+operator|.
+name|helpers
+operator|.
+name|KsmKeyInfo
+import|;
+end_import
+
+begin_comment
+comment|/**  * A class that encapsulates OzoneKey.  */
+end_comment
+
+begin_class
+DECL|class|OzoneKey
+specifier|public
+class|class
+name|OzoneKey
+block|{
+comment|/**    * Name of the Volume the Key belongs to.    */
+DECL|field|volumeName
+specifier|private
+specifier|final
+name|String
+name|volumeName
+decl_stmt|;
+comment|/**    * Name of the Bucket the Key belongs to.    */
+DECL|field|bucketName
+specifier|private
+specifier|final
+name|String
+name|bucketName
+decl_stmt|;
+comment|/**    * Name of the Key.    */
+DECL|field|keyName
+specifier|private
+specifier|final
+name|String
+name|keyName
+decl_stmt|;
+comment|/**    * Name of the Container the Key resides in.    */
+DECL|field|containerName
+specifier|private
+specifier|final
+name|String
+name|containerName
+decl_stmt|;
+comment|/**    * Name of the block id SCM assigned for the key.    */
+DECL|field|blockID
+specifier|private
+specifier|final
+name|String
+name|blockID
+decl_stmt|;
+comment|/**    * Size of the data.    */
+DECL|field|dataSize
+specifier|private
+specifier|final
+name|long
+name|dataSize
+decl_stmt|;
+comment|/**    * Constructs OzoneKey from KsmKeyInfo.    *    * @param ksmKeyInfo    */
+DECL|method|OzoneKey (KsmKeyInfo ksmKeyInfo)
+specifier|public
+name|OzoneKey
+parameter_list|(
+name|KsmKeyInfo
+name|ksmKeyInfo
+parameter_list|)
+block|{
+name|this
+operator|.
+name|volumeName
+operator|=
+name|ksmKeyInfo
+operator|.
+name|getVolumeName
+argument_list|()
+expr_stmt|;
+name|this
+operator|.
+name|bucketName
+operator|=
+name|ksmKeyInfo
+operator|.
+name|getBucketName
+argument_list|()
+expr_stmt|;
+name|this
+operator|.
+name|keyName
+operator|=
+name|ksmKeyInfo
+operator|.
+name|getKeyName
+argument_list|()
+expr_stmt|;
+name|this
+operator|.
+name|containerName
+operator|=
+name|ksmKeyInfo
+operator|.
+name|getContainerName
+argument_list|()
+expr_stmt|;
+name|this
+operator|.
+name|blockID
+operator|=
+name|ksmKeyInfo
+operator|.
+name|getBlockID
+argument_list|()
+expr_stmt|;
+name|this
+operator|.
+name|dataSize
+operator|=
+name|ksmKeyInfo
+operator|.
+name|getDataSize
+argument_list|()
+expr_stmt|;
+block|}
+comment|/**    * Returns Volume Name associated with the Key.    *    * @return volumeName    */
+DECL|method|getVolumeName ()
+specifier|public
+name|String
+name|getVolumeName
+parameter_list|()
+block|{
+return|return
+name|volumeName
+return|;
+block|}
+comment|/**    * Returns Bucket Name associated with the Key.    *    * @return bucketName    */
+DECL|method|getBucketName ()
+specifier|public
+name|String
+name|getBucketName
+parameter_list|()
+block|{
+return|return
+name|bucketName
+return|;
+block|}
+comment|/**    * Returns the Key Name.    *    * @return keyName    */
+DECL|method|getKeyName ()
+specifier|public
+name|String
+name|getKeyName
+parameter_list|()
+block|{
+return|return
+name|keyName
+return|;
+block|}
+comment|/**    * Returns Container Name associated with the Key.    *    * @return containerName    */
+DECL|method|getContainerName ()
+specifier|public
+name|String
+name|getContainerName
+parameter_list|()
+block|{
+return|return
+name|containerName
+return|;
+block|}
+comment|/**    * Returns BlockID associated with the Key.    *    * @return blockID    */
+DECL|method|getBlockID ()
+specifier|public
+name|String
+name|getBlockID
+parameter_list|()
+block|{
+return|return
+name|blockID
+return|;
+block|}
+comment|/**    * Returns the size of the data.    *    * @return dataSize    */
+DECL|method|getDataSize ()
+specifier|public
+name|long
+name|getDataSize
+parameter_list|()
+block|{
+return|return
+name|dataSize
+return|;
+block|}
+block|}
+end_class
+
+end_unit
+
