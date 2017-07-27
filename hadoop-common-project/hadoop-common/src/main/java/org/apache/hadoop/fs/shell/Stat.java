d225d587a58afb0514ed726b4be04ca16fc452bb
Yongjun Zhang <yzhang@cloudera.com>
2017-07-27 16:48:24 -0700

    HDFS-12190. Enable 'hdfs dfs -stat' to display access time. Contributed by Yongjun Zhang.
    Former-commit-id: c6330f22a5e5c2370bab885f9bea4bf8f5e9cf44

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/shell/Stat.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/shell/Stat.java
@@ -111,7 +111,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Print statistics about path in specified format.  * Format sequences:<br>  *   %a: Permissions in octal<br>  *   %A: Permissions in symbolic style<br>  *   %b: Size of file in bytes<br>  *   %F: Type<br>  *   %g: Group name of owner<br>  *   %n: Filename<br>  *   %o: Block size<br>  *   %r: replication<br>  *   %u: User name of owner<br>  *   %y: UTC date as&quot;yyyy-MM-dd HH:mm:ss&quot;<br>  *   %Y: Milliseconds since January 1, 1970 UTC<br>  * If the format is not specified, %y is used by default.  */
+comment|/**  * Print statistics about path in specified format.  * Format sequences:<br>  *   %a: Permissions in octal<br>  *   %A: Permissions in symbolic style<br>  *   %b: Size of file in bytes<br>  *   %F: Type<br>  *   %g: Group name of owner<br>  *   %n: Filename<br>  *   %o: Block size<br>  *   %r: replication<br>  *   %u: User name of owner<br>  *   %x: atime UTC date as&quot;yyyy-MM-dd HH:mm:ss&quot;<br>  *   %X: atime Milliseconds since January 1, 1970 UTC<br>  *   %y: mtime UTC date as&quot;yyyy-MM-dd HH:mm:ss&quot;<br>  *   %Y: mtime Milliseconds since January 1, 1970 UTC<br>  * If the format is not specified, %y is used by default.  */
 end_comment
 
 begin_class
@@ -210,15 +210,19 @@ literal|"name (%n), block size (%o), replication (%r), user name"
 operator|+
 name|NEWLINE
 operator|+
-literal|"of owner (%u), modification date (%y, %Y)."
+literal|"of owner (%u), access date (%x, %X)."
 operator|+
 name|NEWLINE
 operator|+
-literal|"%y shows UTC date as \"yyyy-MM-dd HH:mm:ss\" and"
+literal|"modification date (%y, %Y)."
 operator|+
 name|NEWLINE
 operator|+
-literal|"%Y shows milliseconds since January 1, 1970 UTC."
+literal|"%x and %y show UTC date as \"yyyy-MM-dd HH:mm:ss\" and"
+operator|+
+name|NEWLINE
+operator|+
+literal|"%X and %Y show milliseconds since January 1, 1970 UTC."
 operator|+
 name|NEWLINE
 operator|+
@@ -575,6 +579,43 @@ argument_list|(
 name|stat
 operator|.
 name|getOwner
+argument_list|()
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'x'
+case|:
+name|buf
+operator|.
+name|append
+argument_list|(
+name|timeFmt
+operator|.
+name|format
+argument_list|(
+operator|new
+name|Date
+argument_list|(
+name|stat
+operator|.
+name|getAccessTime
+argument_list|()
+argument_list|)
+argument_list|)
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'X'
+case|:
+name|buf
+operator|.
+name|append
+argument_list|(
+name|stat
+operator|.
+name|getAccessTime
 argument_list|()
 argument_list|)
 expr_stmt|;
