19a6889a9ff10861867a00cf6241c7ef6dc84df4
Rohith Sharma K S <rohithsharmaks@apache.org>
2017-08-18 13:32:36 +0530

    YARN-7038. [Atsv2 Security] CollectorNodemanagerProtocol RPC interface doesn't work when service authorization is enabled. Contributed by Varun Saxena.
    Former-commit-id: 32188d32954d94ec2efeec2b2fcc5b2abff4c1ea

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-api/src/main/java/org/apache/hadoop/yarn/conf/YarnConfiguration.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-api/src/main/java/org/apache/hadoop/yarn/conf/YarnConfiguration.java
@@ -6149,6 +6149,15 @@ name|YARN_SECURITY_SERVICE_AUTHORIZATION_APPLICATIONHISTORY_PROTOCOL
 init|=
 literal|"security.applicationhistory.protocol.acl"
 decl_stmt|;
+specifier|public
+specifier|static
+specifier|final
+name|String
+DECL|field|YARN_SECURITY_SERVICE_AUTHORIZATION_COLLECTOR_NODEMANAGER_PROTOCOL
+name|YARN_SECURITY_SERVICE_AUTHORIZATION_COLLECTOR_NODEMANAGER_PROTOCOL
+init|=
+literal|"security.collector-nodemanager.protocol.acl"
+decl_stmt|;
 comment|/** No. of milliseconds to wait between sending a SIGTERM and SIGKILL    * to a running container */
 DECL|field|NM_SLEEP_DELAY_BEFORE_SIGKILL_MS
 specifier|public
