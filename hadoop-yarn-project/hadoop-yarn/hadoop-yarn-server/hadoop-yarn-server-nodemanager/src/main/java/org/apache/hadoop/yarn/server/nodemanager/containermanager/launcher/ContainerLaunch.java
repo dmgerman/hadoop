7b0818550c4d87fcd48e85b9f324f620be9da1b0
Eric Payne <epayne@apache.org>
2017-09-13 14:38:58 -0500

    Revert 'YARN-4727. Unable to override the $HADOOP_CONF_DIR env variable for container. Contributed by Jason Lowe.'
    This reverts commit 485f017e6e03082c94484a2fc3ffe764a7505ab1 [formerly 729d05f5293acf63e7e4aa3bfbf29b999c9a2906].
    
    
    Former-commit-id: a3c44195bed724c02bb76859fe2690d6a9e8f2e9

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/containermanager/launcher/ContainerLaunch.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/containermanager/launcher/ContainerLaunch.java
@@ -6520,7 +6520,7 @@ name|toString
 argument_list|()
 argument_list|)
 expr_stmt|;
-name|putEnvIfAbsent
+name|putEnvIfNotNull
 argument_list|(
 name|environment
 argument_list|,
@@ -6530,6 +6530,18 @@ name|HADOOP_CONF_DIR
 operator|.
 name|name
 argument_list|()
+argument_list|,
+name|System
+operator|.
+name|getenv
+argument_list|(
+name|Environment
+operator|.
+name|HADOOP_CONF_DIR
+operator|.
+name|name
+argument_list|()
+argument_list|)
 argument_list|)
 expr_stmt|;
 if|if
