5c28b07d83f1e889f8df0e4853350392632b545b
Anu Engineer <aengineer@apache.org>
2017-09-23 10:47:17 -0700

    HDFS-12535. Change the Scope of the Class DFSUtilClient to Private. Contributed by  Bharat Viswanadham.
    Former-commit-id: e9a18b625fe2350f1de20452ecf92976187832ad

--- hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/DFSUtilClient.java
+++ hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/DFSUtilClient.java
@@ -80,6 +80,20 @@ name|apache
 operator|.
 name|hadoop
 operator|.
+name|classification
+operator|.
+name|InterfaceAudience
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|hadoop
+operator|.
 name|conf
 operator|.
 name|Configuration
@@ -1015,6 +1029,10 @@ import|;
 end_import
 
 begin_class
+annotation|@
+name|InterfaceAudience
+operator|.
+name|Private
 DECL|class|DFSUtilClient
 specifier|public
 class|class
