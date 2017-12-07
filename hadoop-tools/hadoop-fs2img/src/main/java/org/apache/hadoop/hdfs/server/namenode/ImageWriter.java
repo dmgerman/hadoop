6f1c8543052bbfd5dd693935d11161bfa44ddb9b
Virajith Jalaparti <virajith@apache.org>
2017-12-07 14:21:24 -0800

    HDFS-12903. [READ] Fix closing streams in ImageWriter
    Former-commit-id: 962b5e722ba86d1c012be11280c6b8fb5e0a2043

--- hadoop-tools/hadoop-fs2img/src/main/java/org/apache/hadoop/hdfs/server/namenode/ImageWriter.java
+++ hadoop-tools/hadoop-fs2img/src/main/java/org/apache/hadoop/hdfs/server/namenode/ImageWriter.java
@@ -1441,7 +1441,7 @@ expr_stmt|;
 block|}
 catch|catch
 parameter_list|(
-name|IOException
+name|Throwable
 name|e
 parameter_list|)
 block|{
@@ -1457,7 +1457,11 @@ name|dirsTmpStream
 argument_list|)
 expr_stmt|;
 throw|throw
+operator|new
+name|IOException
+argument_list|(
 name|e
+argument_list|)
 throw|;
 block|}
 try|try
