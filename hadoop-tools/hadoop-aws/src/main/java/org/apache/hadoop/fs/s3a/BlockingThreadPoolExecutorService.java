b2935b02813dea43b3cbe09c3fc23028db6f152a
Kai Zheng <zhengkai.zk@alibaba-inc.com>
2017-12-14 11:03:56 +0800

    Revert "HADOOP-15039. Move SemaphoredDelegatingExecutor to hadoop-common. Contributed by Genmao Yu"
    This reverts commit 479d6a5792262c977025c26fd4960574b0db6847
    
    
    Former-commit-id: 28792b6b7f137df1db58496f27de23bbe99cdfd6

--- hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/BlockingThreadPoolExecutorService.java
+++ hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/BlockingThreadPoolExecutorService.java
@@ -4,7 +4,7 @@ comment|/**  * Licensed to the Apache Software Foundation (ASF) under one  * or 
 end_comment
 
 begin_package
-DECL|package|org.apache.hadoop.util
+DECL|package|org.apache.hadoop.fs.s3a
 package|package
 name|org
 operator|.
@@ -12,7 +12,9 @@ name|apache
 operator|.
 name|hadoop
 operator|.
-name|util
+name|fs
+operator|.
+name|s3a
 package|;
 end_package
 
@@ -162,7 +164,6 @@ name|InterfaceAudience
 operator|.
 name|Private
 DECL|class|BlockingThreadPoolExecutorService
-specifier|public
 specifier|final
 class|class
 name|BlockingThreadPoolExecutorService
@@ -323,7 +324,6 @@ return|;
 block|}
 comment|/**    * Get a named {@link ThreadFactory} that just builds daemon threads.    *    * @param prefix name prefix for all threads created from the factory    * @return a thread factory that creates named, daemon threads with    * the supplied exception handler and normal priority    */
 DECL|method|newDaemonThreadFactory (final String prefix)
-specifier|public
 specifier|static
 name|ThreadFactory
 name|newDaemonThreadFactory
