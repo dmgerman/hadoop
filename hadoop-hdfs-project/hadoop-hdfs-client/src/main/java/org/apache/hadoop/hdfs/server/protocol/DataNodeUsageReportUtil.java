fc63d373020503ea6ede7a5ad25b57eff3d6d5f0
Takanobu Asanuma <tasanuma@yahoo-corp.jp>
2018-04-25 17:01:14 +0900

    HADOOP-15404. Remove multibyte characters in DataNodeUsageReportUtil
    Signed-off-by: Akira Ajisaka <aajisaka@apache.org>
    
    Former-commit-id: 6f655aed3b83899163740c98bac8d4919d65cde3

--- hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/server/protocol/DataNodeUsageReportUtil.java
+++ hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/server/protocol/DataNodeUsageReportUtil.java
@@ -63,7 +63,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * This class is helper class to generate a live usage report by calculating  * the delta betweenââcurrent DataNode usage metrics and the usage metrics  * captured at the time of the last report.  */
+comment|/**  * This class is helper class to generate a live usage report by calculating  * the delta between current DataNode usage metrics and the usage metrics  * captured at the time of the last report.  */
 end_comment
 
 begin_class
