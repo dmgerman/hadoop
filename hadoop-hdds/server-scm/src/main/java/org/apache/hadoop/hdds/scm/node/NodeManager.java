83c8e8d2d06f69d263096a83fa521b2d1714fd69
Xiaoyu Yao <xyao@apache.org>
2018-06-27 13:28:00 -0700

    HDDS-194. Remove NodePoolManager and node pool handling from SCM. Contributed by Elek Marton.
    Former-commit-id: 56a4cdb9804daea7164155a5b1b4eba44a11b705

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/node/NodeManager.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/node/NodeManager.java
@@ -307,12 +307,6 @@ name|DatanodeDetails
 name|datanodeDetails
 parameter_list|)
 function_decl|;
-comment|/**    * Returns the NodePoolManager associated with the NodeManager.    * @return NodePoolManager    */
-DECL|method|getNodePoolManager ()
-name|NodePoolManager
-name|getNodePoolManager
-parameter_list|()
-function_decl|;
 comment|/**    * Wait for the heartbeat is processed by NodeManager.    * @return true if heartbeat has been processed.    */
 annotation|@
 name|VisibleForTesting
