3a65e460b0387a5cc6e13665bcaff00d4ce98a91
Anu Engineer <aengineer@apache.org>
2018-08-22 18:55:14 -0700

    HDDS-356. Support ColumnFamily based RockDBStore and TableStore. Contributed by Anu Engineer.
    Former-commit-id: b021249ac84abe31c9d30d73ed483bea2acdbaab

---
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/utils/db/DBStore.java
@@ -0,0 +1,153 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  *  with the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  *  */
+end_comment
+
+begin_package
+DECL|package|org.apache.hadoop.utils.db
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|hadoop
+operator|.
+name|utils
+operator|.
+name|db
+package|;
+end_package
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|hadoop
+operator|.
+name|classification
+operator|.
+name|InterfaceStability
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|io
+operator|.
+name|IOException
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|ArrayList
+import|;
+end_import
+
+begin_comment
+comment|/**  * The DBStore interface provides the ability to create Tables, which store  * a specific type of Key-Value pair. Some DB interfaces like LevelDB will not  * be able to do this. In those case a Table creation will map to a default  * store.  *  */
+end_comment
+
+begin_interface
+annotation|@
+name|InterfaceStability
+operator|.
+name|Evolving
+DECL|interface|DBStore
+specifier|public
+interface|interface
+name|DBStore
+extends|extends
+name|AutoCloseable
+block|{
+comment|/**    * Gets an existing TableStore.    *    * @param name - Name of the TableStore to get    * @return - TableStore.    * @throws IOException on Failure    */
+DECL|method|getTable (String name)
+name|Table
+name|getTable
+parameter_list|(
+name|String
+name|name
+parameter_list|)
+throws|throws
+name|IOException
+function_decl|;
+comment|/**    * Lists the Known list of Tables in a DB.    *    * @return List of Tables, in case of Rocks DB and LevelDB we will return at    * least one entry called DEFAULT.    * @throws IOException on Failure    */
+DECL|method|listTables ()
+name|ArrayList
+argument_list|<
+name|Table
+argument_list|>
+name|listTables
+parameter_list|()
+throws|throws
+name|IOException
+function_decl|;
+comment|/**    * Compact the entire database.    *    * @throws IOException on Failure    */
+DECL|method|compactDB ()
+name|void
+name|compactDB
+parameter_list|()
+throws|throws
+name|IOException
+function_decl|;
+comment|/**    * Moves a key from the Source Table to the destination Table.    *    * @param key - Key to move.    * @param source - Source Table.    * @param dest - Destination Table.    * @throws IOException on Failure    */
+DECL|method|move (byte[] key, Table source, Table dest)
+name|void
+name|move
+parameter_list|(
+name|byte
+index|[]
+name|key
+parameter_list|,
+name|Table
+name|source
+parameter_list|,
+name|Table
+name|dest
+parameter_list|)
+throws|throws
+name|IOException
+function_decl|;
+comment|/**    * Moves a key from the Source Table to the destination Table and updates the    * destination to the new value.    *    * @param key - Key to move.    * @param value - new value to write to the destination table.    * @param source - Source Table.    * @param dest - Destination Table.    * @throws IOException on Failure    */
+DECL|method|move (byte[] key, byte[] value, Table source, Table dest)
+name|void
+name|move
+parameter_list|(
+name|byte
+index|[]
+name|key
+parameter_list|,
+name|byte
+index|[]
+name|value
+parameter_list|,
+name|Table
+name|source
+parameter_list|,
+name|Table
+name|dest
+parameter_list|)
+throws|throws
+name|IOException
+function_decl|;
+comment|/**    * Returns an estimated count of keys in this DB.    *    * @return long, estimate of keys in the DB.    */
+DECL|method|getEstimatedKeyCount ()
+name|long
+name|getEstimatedKeyCount
+parameter_list|()
+throws|throws
+name|IOException
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
